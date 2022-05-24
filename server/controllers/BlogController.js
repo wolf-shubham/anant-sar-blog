befdcccb9d98533c15e90dcfce3b3b68fc89f588
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-05-24 12:53:29 +0530

    get blogs by category

--- server/controllers/BlogController.js
+++ server/controllers/BlogController.js
@@ -25,4 +25,18 @@ const getAllBlogs = async (req, res) => {
     }
 }
 
-module.exports = { createBlog, getAllBlogs }
+const getBlogsByCategory = async (req, res) => {
+    try {
+        const categoryName = req.params.category
+        // first alphabet og category should be capital
+        const category = categoryName.charAt(0).toUpperCase() + categoryName.slice(1)
+
+        const blogs = await Blog.find({ categories: category })
+        return res.status(200).json({ message: 'get blogs by category success', blogs })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: 'Internal Server Error', error })
+    }
+}
+
+module.exports = { createBlog, getAllBlogs, getBlogsByCategory }
--- server/routes/BlogRoutes.js
+++ server/routes/BlogRoutes.js
@@ -1,9 +1,11 @@
-const { createBlog, getAllBlogs } = require('../controllers/BlogController')
+const { createBlog, getAllBlogs, getBlogsByCategory } = require('../controllers/BlogController')
 
 const route = require('express')()
 
 route.post('/createblog', createBlog)
 
 route.get('/getallblogs', getAllBlogs)
+
+route.get('/getblogsbycategory/:category', getBlogsByCategory)
 
 module.exports = route
